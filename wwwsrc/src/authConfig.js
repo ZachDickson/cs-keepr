3b7d6a86ba2f881893334869c5800682fd55e853
Zach Dickson <z.dickson10@gmail.com>
2020-05-07 14:00:04 -0600

    auth

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "dev-1k3ferig.auth0.com";
-export const audience = "https://keepr99.com";
+export const audience = "keepr99";
 export const clientId = "XmEgMY7mEYYWhjMezy8l60dv2j8cMVYI";
